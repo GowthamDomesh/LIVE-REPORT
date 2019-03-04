38db693c0e8b5c2772ab9a89373c90189b89e06b
Gowtham Domesh <domeshgowtham@gmail.com>
2019-03-04 22:39:10 +0530

    Live - report : architecture and base set up for page completed

---
+++ src/components/Report-page.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import '../App.css';
+import { Row, Col, Button } from 'react-bootstrap';
+
+class ReportPage extends Component {
+  render() {
+    return (
+      <div class="report-container">
+            <div className="ofs-left-pane">
+                <span className="ofs-label">OFS</span>
+            </div>
+            <div className="login-container">
+                <span className="login-label">Login | Signup</span>
+            </div>
+            <div className="live-report-tracker-container">
+                <div className="live-report-inner-container">
+                    <span xs={6} className="live-report-label">
+                        Live Report
+                    </span>
+                    <span xs={6} className="report-selection float-right">
+                        <Button variant="primary" className="day-report-btn">Day Report</Button>
+                        <Button variant="light" className="sprint-report-btn">Sprint Report</Button>
+                    </span>  
+                </div>
+            </div>
+        Report Content and filters goes here!!!
+        {/* <Button variant="primary">Hello</Button> */}
+      </div>
+    );
+  }
+}
+
+export default ReportPage;
