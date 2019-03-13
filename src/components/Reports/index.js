8898647354edf4a94be9450d581a29a79c72a929
sgurusamy <sgurusamy@overstock.com>
2019-03-13 19:41:47 +0530

    Added changes to Reports

--- src/components/Reports/index.js
+++ src/components/Reports/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './reports.scss';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, Form } from 'react-bootstrap';
 import HealthCard from '../HealthCard';
 
 class ReportPage extends Component {
@@ -38,7 +38,36 @@ class ReportPage extends Component {
                 </div>
                 <div className="filters-container">
                     <div className="inner-content-container">
-                        ...Filter buttons goes here...
+                        <Form>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="name@example.com" />
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Example select</Form.Label>
+                                <Form.Control as="select">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlSelect2">
+                                <Form.Label>Example multiple select</Form.Label>
+                                <Form.Control as="select" multiple>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Example textarea</Form.Label>
+                                <Form.Control as="textarea" rows="3" />
+                            </Form.Group>
+                        </Form>;
                     </div>
                 </div>
                 <HealthCard type={reportType} />
