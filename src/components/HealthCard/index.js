18365186d75ea175523f5eddd7067f2593a32e8f
Gowtham Nagarajan <gnagarajan@overstock.com>
2019-03-20 17:00:00 +0530

    added live data to the report card

--- src/components/HealthCard/index.js
+++ src/components/HealthCard/index.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 import { Chart } from "react-charts";
 import './HealthReport.scss';
+import axios from 'axios';
 
-class HealthCard extends Component {
+class HealthCard extends Component {  
   constructor (props) {
     super(props);
     this.state = {};
+    this.reportDetails = {};
+  }
+  componentDidMount() {
+    axios.get(`http://localhost:8000/api/reports`)
+      .then(res => {
+        console.log(res);
+        this.reportDetails = res.data;
+        this.setState({ reportDetail: this.reportDetails});
+      })
   }
   render() {
     const { type } = this.props;
+    const { reportDetail } = this.state;
+    console.log(reportDetail);
+    const roundOffValues = (value) => {
+      return parseFloat(Math.round(value * 100) / 100).toFixed(2) + '%'
+    };
+
     return (
       <div className="health-report-container">
           <div className="report-header">Health Report on {type}</div>
@@ -23,27 +39,27 @@ class HealthCard extends Component {
               <div className="table-content">
                 <div className="inner-txt">Commitment Index</div>
                 <div className="inner-txt">Sprint</div>
-                <div className="inner-txt">70%</div>
+                <div className="inner-txt">{roundOffValues(this.reportDetails.commitmentIndex)}</div>
               </div>
               <div className="table-content">
                 <div className="inner-txt">Availabel Capacity</div>
                 <div className="inner-txt">Sprint</div>
-                <div className="inner-txt">70%</div>
+                <div className="inner-txt">{roundOffValues(this.reportDetails.availableVsPlannedCapacity)}</div>
               </div>
               <div className="table-content">
                 <div className="inner-txt">Planed Vs Acutual</div>
                 <div className="inner-txt">Sprint</div>
-                <div className="inner-txt">80%</div>
+                <div className="inner-txt">{roundOffValues(this.reportDetails.plannedVsActual)}</div>
               </div>
               <div className="table-content">
                 <div className="inner-txt">Resource Utilize</div>
                 <div className="inner-txt">Sprint</div>
-                <div className="inner-txt">70%</div>
+                <div className="inner-txt">{roundOffValues(this.reportDetails.resourceUtilization)}</div>
               </div>
               <div className="table-content">
                 <div className="inner-txt">QA Test Effectiveness</div>
                 <div className="inner-txt">Sprint</div>
-                <div className="inner-txt">90%</div>
+                <div className="inner-txt">{roundOffValues(this.reportDetails.qaTestEffectiveness)}</div>
               </div>
             </div>
             <div className="health-chart">
